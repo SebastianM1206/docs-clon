8e5d83da86d00520486290818ba63c23fe4c3e8e
Sebastian <semega1206@gmail.com>
2025-04-17 23:11:02 -0500

    Tiptap Extensions installed

--- src/app/documents/[documentId]/editor.tsx
+++ src/app/documents/[documentId]/editor.tsx
@@ -2,7 +2,15 @@
 
 import React from 'react'
 import { useEditor, EditorContent } from '@tiptap/react'
+import TaskItem from '@tiptap/extension-task-item'
+import TaskList from '@tiptap/extension-task-list'
 import StarterKit from '@tiptap/starter-kit'
+import Table from '@tiptap/extension-table'
+import TableCell from '@tiptap/extension-table-cell'
+import TableHeader from '@tiptap/extension-table-header'
+import TableRow from '@tiptap/extension-table-row'
+import Image from '@tiptap/extension-image'
+import ImageResize from 'tiptap-extension-resize-image'
 
 
 
@@ -11,11 +19,36 @@ export const Editor = () => {
     const editor = useEditor({
         editorProps: {
             attributes: { 
-                style: 'padding-lef: 56px; padding-right: 56px; ',
+                style: 'padding-left: 56px; padding-right: 56px; ',
                 class: ' focus:outline-none print:border-0 bg-white border border-[#C7C7C7] flex flex-col min-h-[1054px] w-[816px] pt-10 pr-14 pb-10 cursor-text dark:prose-invert max-w-none' }, // This is the class that will be applied to the editor content area.
         },
-        extensions: [StarterKit],
-        content: '<p>Hello World! 🌎️</p>',
+        extensions: [StarterKit,TaskList,
+            TaskItem.configure({
+              nested: true,
+            }),
+            Table.configure({
+                resizable: true,
+              }),
+              TableRow,
+              TableHeader,
+              TableCell, 
+            Image, ImageResize    ],
+        content: `
+        <table>
+          <tbody>
+            <tr>
+              <th>Name</th>
+              <th colspan="3">Description</th>
+            </tr>
+            <tr>
+              <td>Cyndi Lauper</td>
+              <td>Singer</td>
+              <td>Songwriter</td>
+              <td>Actress</td>
+            </tr>
+          </tbody>
+        </table>
+      `,
       })
   return (
     <div className='size-full overflow-x-auto px bg-[#F9FBFD] px-4 print:p-0 print:bg-white print:overflow-visible'> 
